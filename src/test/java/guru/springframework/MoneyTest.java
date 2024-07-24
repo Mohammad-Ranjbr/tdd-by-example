e7442eabf3f0c109fdbe7f209aa5bc8dff66875c
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-07-24 08:10:47 +0330

    write testMultiplication test

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,4 +1,16 @@
 package guru.springframework;
 
+import com.springframework.Dollar;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoneyTest {
+
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        Assertions.assertEquals(10,five.amount);
+    }
+
 }
