48277a65fc1bd2cc5b9c55a6e79ea3a5f19b3b9e
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-07-24 11:20:53 +0330

    add dollar , franc and times method Money class

--- src/main/java/com/springframework/Money.java
+++ src/main/java/com/springframework/Money.java
@@ -1,14 +1,26 @@
 package com.springframework;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    // An abstract method is a method without a body that only defines the method signature.
+    // These methods must be implemented in child classes.
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     // The Java equals method only checks the equality of the memory location of two objects
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(object.getClass());
     }
 
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,7 +1,7 @@
 package guru.springframework;
 
-import com.springframework.Dollar;
 import com.springframework.Franc;
+import com.springframework.Money;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,32 +9,29 @@ public class MoneyTest {
 
     @Test
     void testMultiplicationDollar() {
-        Dollar five = new Dollar(5);
-        Dollar proudct = five.times(2);
-        Assertions.assertEquals(new Dollar(10),proudct);
-        proudct = five.times(3);
-        Assertions.assertEquals(new Dollar(15),proudct);
+        Money five = Money.dollar(5);
+        Assertions.assertEquals(Money.dollar(10),five.times(2));
+        Assertions.assertEquals(Money.dollar(15),five.times(3));
     }
 
     @Test
     void testEqualityDollar() {
-        Assertions.assertEquals(new Dollar(5),new Dollar(5));
-        Assertions.assertNotEquals(new Dollar(5),new Dollar(8));
+        Assertions.assertEquals(Money.dollar(5),Money.dollar(5));
+        Assertions.assertNotEquals(Money.dollar(5),Money.dollar(8));
+        Assertions.assertNotEquals(Money.dollar(5),Money.franc(5));
     }
 
     @Test
     void testMultiplicationFranc() {
-        Franc five = new Franc(5);
-        Franc proudct = five.times(2);
-        Assertions.assertEquals(new Franc(10),proudct);
-        proudct = five.times(3);
-        Assertions.assertEquals(new Franc(15),proudct);
+        Money five = Money.franc(5);
+        Assertions.assertEquals(Money.franc(10),five.times(2));
+        Assertions.assertEquals(Money.franc(15),five.times(3));
     }
 
     @Test
     void testEqualityFranc() {
-        Assertions.assertEquals(new Franc(5),new Franc(5));
-        Assertions.assertNotEquals(new Franc(5),new Franc(8));
+        Assertions.assertEquals(Money.franc(5),new Franc(5));
+        Assertions.assertNotEquals(Money.franc(5),new Franc(8));
     }
 
 }
