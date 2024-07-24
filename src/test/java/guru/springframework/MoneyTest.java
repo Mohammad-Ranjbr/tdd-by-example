34713a999ef4df77079514938bd5b8899e1ff779
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-07-24 08:23:50 +0330

    add new assertEquals

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,8 +9,10 @@ public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assertions.assertEquals(10,five.amount);
+        Dollar proudct = five.times(2);
+        Assertions.assertEquals(10,proudct.amount);
+        proudct = five.times(3);
+        Assertions.assertEquals(15,proudct.amount);
     }
 
 }
